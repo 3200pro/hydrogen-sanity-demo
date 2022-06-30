5d8aeae74d1a052a856691ecf63cf16e9ff90607
3200pro <info@3200.pro>
2022-06-30 15:37:25 -0500

    Add @netlify/hydrogen-platform Dependency

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import {defineConfig} from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
 import netlifyPlugin from '@netlify/hydrogen-platform/plugin';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [hydrogen(), netlifyPlugin()],
   optimizeDeps: {include: ['@headlessui/react']},
